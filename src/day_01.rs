9a0ae46c35363f5e1f78b393c7d634fdb71c0556
Anozireth <anozireth@gmail.com>
2024-12-04 15:36:42 -0800

    Day 1, part 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -41,3 +41,40 @@ pub fn part1() -> Result<()> {
 
     Ok(())
 }
+
+pub fn part2() -> Result<()> {
+    let file = File::open("inputs/day_01.txt").expect("File not found");
+    let mut reader = BufReader::new(file);
+
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+
+    let mut left_locations = Vec::<u32>::new();
+    let mut right_locations = Vec::<u32>::new();
+
+    for (i, token) in buf
+        .split(&[' ', '\n'])
+        .filter(|s| !s.is_empty())
+        .map(|s| s.trim())
+        .enumerate()
+    {
+        if i % 2 == 0 {
+            left_locations.push(token.parse().unwrap());
+        } else {
+            right_locations.push(token.parse().unwrap());
+        }
+    }
+
+    left_locations.sort();
+    right_locations.sort();
+
+    let mut similarity_score = 0;
+    for left in left_locations {
+        let right_count = right_locations.iter().filter(|&r| *r == left).count();
+        similarity_score += left * right_count as u32;
+    }
+
+    println!("Day 1, Part 2: Similarity Score: {}", similarity_score);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use anyhow::Result;
 fn main() -> Result<()> {
     println!("Welcome to Advent of Code 2024!");
     day_01::part1()?;
-
+    day_01::part2()?;
     Ok(())
 }
